f78310bacbad5eb56e2524fdbcf624fd58ef34da
davidv410 <davidvrgoc99@gmail.com>
2026-09-15 16:51:06 +0200

    reservation info display

--- src/pages/reservations.tsx
+++ src/pages/reservations.tsx
@@ -2,6 +2,7 @@ import {useReservation} from "@/features/reservations/hooks/useReservation.ts";
 import {useCancelReservation} from "@/features/reservations/hooks/useCancelReservation.ts";
 import {useAuth} from "@/features/auth/context.tsx";
 import { Header } from "@/components/Header";
+import { formatDateTime } from "@/lib/formatDate";
 
 export const Reservations = () => {
 
@@ -23,12 +24,14 @@ export const Reservations = () => {
             {
                 (data ?? []).map(item => (
                     <div className="border">
-                        <p>{item.seats.row} - {item.seats.number}</p>
-                        <p>{item.seats.price} KM</p>
-                        { item.reservations.status === 'cancelled' ?
+                        <p>Reservation for: {item.userName} ({item.userEmail})</p>
+                        <p>{formatDateTime(item.startTime)}</p>
+                        <p>{item.seatRow} - {item.seatNumber}</p>
+                        <p>{item.seatPrice} KM</p>
+                        { item.reservationStatus === 'cancelled' ?
                         <p className="text-gray-500">reservation has been cancelled</p>
                         :
-                        <button className="cursor-pointer text-red-500" onClick={() => mutate(item.reservations.id)}>CANCEL RESERVATION</button>
+                        <button className="cursor-pointer text-red-500" onClick={() => mutate(item.reservationId)}>CANCEL RESERVATION</button>
                         }
                     </div>
                 ))
